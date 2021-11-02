e1108c14e9bb19dddd8176f6bcaa537a1df48046
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-11-02 11:55:10 -0700

    Views done, ViewModels started

--- BookStoreApp/ViewModel/MainViewModel.cs
+++ BookStoreApp/ViewModel/MainViewModel.cs
@@ -16,11 +16,13 @@ namespace BookStoreApp.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
+            
             ////if (IsInDesignMode)
             ////{
             ////    // Code runs in Blend --> create design time data.
